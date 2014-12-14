97305f4bd53ff1806c5e00055beac7affcd0cf0f
Devon Stewart <blast@hardchee.se>
2014-12-14 15:43:20 -0800

    4: Parse an entire log file, filtering on recognized error messages

--- src/Log/LogAnalysis.hs
+++ src/Log/LogAnalysis.hs
@@ -15,3 +15,11 @@ parseMessage = work . words
 --              E 10 1418592417 This is an error with a severity of 10
           work ("E" : lvl' : ts' : xs) | (lvl, ts) <- (read lvl', read ts') = Right $ LogMessage (Error lvl) ts $ unwords xs
           work xs = Left $ unwords xs
+
+validMessagesOnly :: [MaybeLogMessage] -> [LogMessage]
+validMessagesOnly lm = foldr accValid [] lm
+    where accValid (Right x) a = x : a
+          accValid (Left _) a = a
+
+parse :: String -> [LogMessage]
+parse s = validMessagesOnly $ parseMessage <$> lines s
