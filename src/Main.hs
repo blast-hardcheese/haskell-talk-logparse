7a3720c3a6c100b8edab3f0c0e76e56a1124b426
Devon Stewart <blast@hardchee.se>
2014-12-15 00:40:37 -0800

    9: Sort messages by date

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,5 @@
 import Control.Applicative ((<$>))
-import Data.List (isInfixOf)
+import Data.List (isInfixOf, sortBy)
 
 import Log.LogAnalysis
 import Log.Log (LogMessage (..), MessageType (..))
@@ -15,7 +15,17 @@ demoteUnimportant :: Int -> LogMessage -> LogMessage
 demoteUnimportant limit lm@(LogMessage (Error lvl) ts msg) | lvl < limit = LogMessage Warning ts msg
 demoteUnimportant _ lm = lm
 
+sortMessages :: [LogMessage] -> [LogMessage]
+sortMessages = sortBy compareMsgs
+  where compareMsgs :: LogMessage -> LogMessage -> Ordering
+        compareMsgs (LogMessage _ l _) (LogMessage _ r _) = compare l r
+
+
 main = do
   logs <- parse <$> readFile "serverlogs.log"
   putStrLn $ unlines $ show <$> filter (contains "AirPort") logs
   putStrLn $ unlines $ show <$> demoteUnimportant 30 <$> filter (isError) logs
+
+  logs <- parse <$> readFile "serverlogs-async.log"
+  putStrLn $ unlines $ fmap show $ filter (isError) $ (demoteUnimportant 30) <$> sortMessages logs
+  putStrLn $ unlines $ fmap show $ sortMessages $ filter (isError) $ (demoteUnimportant 30) <$> logs
