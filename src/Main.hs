45b9833e57a94f6211096ea49f491b5c1b5d1989
Devon Stewart <blast@hardchee.se>
2014-12-15 00:18:29 -0800

    6: Filter logs that contain the word "AirPort"

--- src/Main.hs
+++ src/Main.hs
@@ -1,7 +1,12 @@
 import Control.Applicative ((<$>))
+import Data.List (isInfixOf)
 
 import Log.LogAnalysis
+import Log.Log (LogMessage (..))
+
+contains :: String -> LogMessage -> Bool
+contains x (LogMessage _ _ m) = x `isInfixOf` m
 
 main = do
   logs <- parse <$> readFile "serverlogs.log"
-  putStrLn $ head $ show <$> logs
+  putStrLn $ unlines $ show <$> filter (contains "AirPort") logs
