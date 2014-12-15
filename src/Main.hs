7047d41e572f0cff4ef101e3297c2c1bc62ffdbf
Devon Stewart <blast@hardchee.se>
2014-12-15 00:19:36 -0800

    7: Only show errors

--- src/Main.hs
+++ src/Main.hs
@@ -2,11 +2,16 @@ import Control.Applicative ((<$>))
 import Data.List (isInfixOf)
 
 import Log.LogAnalysis
-import Log.Log (LogMessage (..))
+import Log.Log (LogMessage (..), MessageType (..))
 
 contains :: String -> LogMessage -> Bool
 contains x (LogMessage _ _ m) = x `isInfixOf` m
 
+isError :: LogMessage -> Bool
+isError (LogMessage (Error _) _ _) = True
+isError _ = False
+
 main = do
   logs <- parse <$> readFile "serverlogs.log"
   putStrLn $ unlines $ show <$> filter (contains "AirPort") logs
+  putStrLn $ unlines $ show <$> filter (isError) logs
